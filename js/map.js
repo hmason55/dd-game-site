571edfd2b191e7f093792258dacd352b198e5151
hmason55 <hmason55@users.noreply.github.com>
2025-05-07 03:20:21 +0000

    deploy: hmason55/dd-game@c071e1927fab257a072488faffa3e29edd9133ae

---
+++ js/map.js
@@ -0,0 +1,19 @@
+﻿window.getBoundingClientRect = (element) => {
+    if (!element) return null;
+    const rect = element.getBoundingClientRect();
+    return {
+        left: rect.left,
+        top: rect.top,
+        width: rect.width,
+        height: rect.height
+    };
+};
+
+window.observeResize = (element, dotnetHelper) => {
+    if (!element) return;
+
+    const resizeObserver = new ResizeObserver(() => {
+        dotnetHelper.invokeMethodAsync('OnResize');
+    });
+    resizeObserver.observe(element);
+};
