0a6d9e6933723f03a9bb78c122d5181ff148220c
hmason55 <hmason55@users.noreply.github.com>
2025-06-15 18:32:17 +0000

    deploy: hmason55/dd-game@33bd38df0e55426454096aacf38e83f304efd812

---
+++ js/utils.js
@@ -0,0 +1,12 @@
+﻿window.copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text).catch(err => console.error('Copy failed', err));
+};
+
+window.pasteFromClipboard = async () => {
+    try {
+        return await navigator.clipboard.readText();
+    } catch (err) {
+        console.error('Paste failed', err);
+        return '';
+    }
+};
