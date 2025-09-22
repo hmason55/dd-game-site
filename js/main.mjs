6a6c0127e95269078ec184f22f5c12cd84ab78d3
hmason55 <hmason55@users.noreply.github.com>
2025-09-22 22:53:55 +0000

    deploy: hmason55/dd-game@9e609f942db851c25c8ef4ff70644a30d647d53d

--- js/main.mjs
+++ js/main.mjs
@@ -4,7 +4,6 @@ import * as animations from './animations.mjs';
 import { particleSystem } from './particles.mjs';
 import * as map from './map.mjs';
 import * as utils from './utils.mjs';
-import { initializePwaSupport } from './pwa.mjs';
 
 window.audioPlayer = audioPlayer;
 Object.assign(window, animations);
@@ -13,4 +12,3 @@ Object.assign(window, map);
 Object.assign(window, utils);
 
 initLoadingMessages();
-initializePwaSupport();
